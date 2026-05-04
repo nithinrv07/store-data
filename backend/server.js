4180d3bf71494bd148667fa856104d8e290bcf77
NITHIN R <nithinr.aids2025@citchennai.net>
2026-05-04 08:20:25 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(cors());
